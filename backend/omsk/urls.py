1f2282dea49ab0a7baac6b99e3e783bebb1f7e33
ZlobinMS <ZlobinMS@admlr.lipetsk.ru>
2022-12-22 20:59:37 +0300

    chore: docker

--- backend/omsk/urls.py
+++ backend/omsk/urls.py
@@ -1,4 +1,5 @@
-from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, re_path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
@@ -27,4 +28,4 @@ urlpatterns = [
    path('api/specifications/', include('apps.specifications.urls')),
 
    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
